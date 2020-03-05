a3540647884559803fe2517483e6a8f872f0c5ad
emmymb20 <emmymb20@byui.edu>
2020-03-04 21:44:41 -0700

    yuh

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -6,48 +6,28 @@
 
 // Listen for the DOM to finish building
 document.addEventListener("DOMContentLoaded", function(){
-    buildModDate();
     const menuButton = document.querySelector("#menu-button");
     menuButton.addEventListener('click', menuButton);
 
     // Variables for Wind Chill function
     let temp = 31;
     let speed = 5; 
-    let feelsLike = document.getElementById('feelsLike');  feelsLike.innerHTML = buildWC(speed, temp);
+    let feelsLike = document.getElementsByClassName('feelsLike');  feelsLike.innerHTML = buildWC(speed, temp);
 
     //Implement the time indicator
     let hour = "7";
     timeBall(hour);
+
+    //Implement background weather image
+    let curCond = "clouds";
+    curCond.toLowerCase();
+    changeSummaryImage(curCond);
 })
 
-// Build the last modified date
-function buildModDate(){
-    const dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let lastMod = new Date(document.lastModified);
-    const dayName = dayArray[lastMod.getDay()];
-    const monthName = monthArray[lastMod.getMonth()];
-    const formattedDate = dayName+", "+lastMod.getDate() +" "+monthName+", "+lastMod.getFullYear();
-    document.querySelector('#modDate').innerText = formattedDate;
-   }
-      
-   // Handles Small Screen Menu
-   const menuButton = document.querySelector("#menuBtn");
-   menuButton.addEventListener('click', menuA);
-     const navList = document.querySelector('#navList');
-     navList.classList.toggle("mobileNav");
-
-
-   function menuA(){
-    const navList = document.querySelector('#navList');
-    navList.classList.toggle("mobileNav");
-  }
-
-    const menubutton = document.querySelector(".ham");
-    menubutton.addEventListener("click", toggleMenu, false);
-
+    // Handles Small Screen Menu
     function toggleMenu() {
     document.querySelector(".navigation").classList.toggle("responsive");
+
 }
 
 /* *************************************
@@ -55,7 +35,7 @@ function buildModDate(){
 ************************************* */   
 
 function buildWC(speed, temp) {
-    let feelsLike = document.getElementById('feelsLike');
+    let feelsLike = document.getElementsByClassName('feelsLike');
 
     // Compute the windchill
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
@@ -78,7 +58,6 @@ function buildWC(speed, temp) {
 *  Time indicator function
 ************************************* */
 function timeBall(hour){
-    console.groupCollapsed("Time Ball");
     // Find all "ball" classes and remove them
     let x = document.querySelectorAll(".ball");
     for (let item of x) {
